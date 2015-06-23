c28f2a1f250094f6515f12bd56607e54d8bff325
ORYZAPAO <oryzapao@gmail.com>
2015-06-24 02:02:26 +0900

    1st Commit

---
+++ write_file.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<fstream> /// fstream
+#include<string>
+
+using namespace std;
+
+/////////////////////////////////////
+int main()
+{
+  string buf;  
+
+  ///ofstream aa("Hello.txt");
+  ofstream aa;
+
+  aa.open("Hello.txt",ios::out );
+  // app	 seek to the end of stream before each write
+  // binary	 open in binary mode
+  // in	 open for reading
+  // out	 open for writing
+  // trunc	 discard the contents of the stream when opening
+  // ate	 seek to the end of stream immediately after open
+
+  aa << "Hello" << endl;
+  aa << "ぞうさん" << endl;
+  
+
+  //  cout << buf << endl;
+  //}
+
+}
+
