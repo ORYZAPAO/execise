dd041dba38507fe924ccd4e78903936d27a5d6e7
ORYZAPAO <oryzapao@gmail.com>
2015-11-08 22:26:33 +0900

    Update Singleton.hpp

--- Singleton.hpp
+++ Singleton.hpp
@@ -1,12 +1,12 @@
 class Singleton {
 private:
-	Singleton() = default;				         //コンストラクタを private に置く
-	~Singleton() = default;					       //デストラクタを private に置く
+	Singleton() = default;				 //コンストラクタを private に置く
+	~Singleton() = default;				 //デストラクタを private に置く
 public:
-	Singleton(const Singleton&) = delete;	 //コピーコンストラクタを delete 指定
-	Singleton& operator=(const Singleton&) = delete;	//コピー代入演算子も delete 指定
-	Singleton(Singleton&&) = delete;			 //ムーブコンストラクタを delete 指定
-	Singleton& operator=(Singleton&&) = delete;		//ムーブ代入演算子も delete 指定
+	Singleton(const Singleton&) = delete;	         //コピーコンストラクタを delete 指定
+	Singleton& operator=(const Singleton&) = delete; //コピー代入演算子も delete 指定
+	Singleton(Singleton&&) = delete;		 //ムーブコンストラクタを delete 指定
+	Singleton& operator=(Singleton&&) = delete;	 //ムーブ代入演算子も delete 指定
 
 	static Singleton& getInstance(){
 		static Singleton inst;			// privateなコンストラクタを呼び出す
