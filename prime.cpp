7a6cf05423f55ed8008dc7e7d512c4453d90197e
ORYZAPAO <oryzapao@gmail.com>
2016-07-03 21:31:38 +0900

    Create prime.cpp

---
+++ prime.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+//include <boost/format.hpp>
+using namespace std;
+
+// Prime 素数を判定
+//
+bool isPrime(int val){
+  int  half_val = (val >> 1); /// val / 2 
+  bool ret_code = false;
+
+  int i;
+  for(i=half_val; i>1; i--){
+    if( (val % i) == 0 ) break;
+  }
+  if(i == 1) ret_code=true;
+  else       ret_code=false;
+
+  return ret_code;
+}
+
+
+//
+//
+int main(){
+  bool result;
+
+  printf("start\n");
+  for(int j=1; j<1000000; j++) {    
+    result = isPrime(j);
+    if( result ) printf("%d ",j);
+  }
+
+}
