3d0c59394b77743165a34dd4445c37806754b5b9
ORYZAPAO <takehitoy@gmail.com>
2016-07-10 16:00:28 +0900

    1st Commit

---
+++ prime/prime2_cuda.cu
@@ -0,0 +1,61 @@
+#include <iostream>
+//include <boost/format.hpp>
+#include<stdio.h>
+#include<array>
+#include<memory>
+
+///#include "make_unique.hpp" /// for c++11
+
+using namespace std;
+
+
+//const int MAX_INT = 1000000;
+const int MAX_INT = 100;
+
+// Prime 素数を判定
+//
+bool isPrime(int val){
+  int  half_val = (val >> 1); /// val / 2 
+  bool ret_code = false;
+
+  int i;
+  for(i=half_val; i>1; i--){
+    if( (val % i) == 0 ) break;
+  }
+  if(i == 1) ret_code=true;
+  else       ret_code=false;
+
+  return ret_code;
+}
+
+
+//
+//
+int main(){
+  array<bool,MAX_INT> result;   // 素数判定結果を格納  
+
+  ///
+  char *pResult;
+  cudaMalloc((void**)&pResult, MAX_INT * sizeof(unsigned int));
+  unique_ptr<unsigned int>  initVal(new unsigned int[MAX_INT]); 
+
+  for(int i=0; i<MAX_INT; i++){
+    initVal= i;
+  }
+
+  
+  // 素数判定
+  printf("start\n");
+  for(int j=1; j<MAX_INT; j++) {    
+    result[j]  = isPrime(j);
+  }
+ 
+  // 結果を表示
+  for(int j=1; j<MAX_INT; j++) {    
+    if( result[j] ) printf("%d ",j);
+  }
+
+  // MemoryFree
+  cudaFree(pResult);
+
+}
