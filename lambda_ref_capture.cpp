5c8555d7108051932b96be9da1664d079fe6b092
oryzapao <takehitoy@gmail.com>
2016-07-17 20:45:44 +0900

    Update

--- lambda_ref_capture.cpp
+++ lambda_ref_capture.cpp
@@ -1,3 +1,6 @@
+//
+// lamda式と、lamda式からの変数の参照（キャプチャ）
+//
 
 #include<iostream>
 
@@ -5,14 +8,32 @@ using namespace std;
 
 int main(){
   // 参照元
-  int val=5;
+  int val;
 
-  // val を参照キャプチャ
-  auto func  = [&val](int m) -> int { return val*m; } ;
+  ///
+  /// val を参照キャプチャ
+  ///
+  cout << "Reference Capture" << endl;
+  val=5;
+  auto func_r  = [&val](int m) -> int { return val*m; } ;
 
-  cout << "val= " << func(10) << endl; // 50
-  cout << "val= " << func(5) << endl;  // 25
+  cout << "val= " << func_r(10) << endl; // 50
+  cout << "val= " << func_r(5) << endl;  // 25
   val =2;
-  cout << "val= " << func(5) << endl;  // 10
+  cout << "val= " << func_r(5) << endl;  // 10
+  cout << endl;
+
+  ///
+  /// val をコピーキャプチャ
+  ///
+  cout << "Copy Capture" << endl;
+  val=5;
+  auto func_c  = [val](int m) -> int { return val*m; } ;
+
+  cout << "val= " << func_c(10) << endl; // 50
+  cout << "val= " << func_c(5) << endl;  // 25
+  val =2;
+  cout << "val= " << func_c(5) << endl;  // 25 != 10(Referrence)
+  cout << endl;
 
 }
