1fb32a4256fe02443f55ebaa71b809a8c05164e0
oryzapao <oryzapao@gmail.com>
2016-07-24 02:48:17 +0900

    Update

--- all_of-any_of.cpp
+++ all_of-any_of.cpp
@@ -12,24 +12,25 @@ int main(){
 
   cout << "Hello" <<endl;
 
+  /// データ一覧
   cout << "dat0 = { ";
   for_each(dat0.begin(), dat0.end(), [](int x){ cout << x << " "; });
   cout << "} " << endl;
 
   /// "全ての"要素が，条件に合致しているか？(all_of)
-  result =  all_of(dat0.begin(), dat0.end(), [](int x){ return x<100; });
-  cout << boost::format("all_of(dat0, x<100 ) =  %i" ) % result  << endl;
+  result =  all_of(dat0.begin(), dat0.end(), [](int x){ return x<100; }); // all_of(dat0, x<100 ) =  1
+  cout << boost::format("all_of(dat0, x<100 ) =  %i" ) % result  << endl;  
 
-  result =  all_of(dat0.begin(), dat0.end(), [](int x){ return x<5; });
+  result =  all_of(dat0.begin(), dat0.end(), [](int x){ return x<5; });   // all_of(dat0, x<5   ) =  0
   cout << boost::format("all_of(dat0, x<5   ) =  %i" ) % result  << endl;
   cout << endl;
 
 
   /// "いずれかの"要素が，条件に合致しているか？(any_of)
-  result =  any_of(dat0.begin(), dat0.end(), [](int x){ return x>5; });
+  result =  any_of(dat0.begin(), dat0.end(), [](int x){ return x>5; });   // any_of(dat0, x>5 ) =  1
   cout << boost::format("any_of(dat0, x>5 ) =  %i" ) % result  << endl;
 
-  result =  any_of(dat0.begin(), dat0.end(), [](int x){ return x>9; });
+  result =  any_of(dat0.begin(), dat0.end(), [](int x){ return x>9; });   // any_of(dat0, x>9 ) =  0
   cout << boost::format("any_of(dat0, x>9 ) =  %i" ) % result  << endl;
   cout << endl;
 
