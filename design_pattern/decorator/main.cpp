1c7883c7586ca564802ffbcdf6109e3d9425f944
oryzapao <takehitoy@gmail.com>
2016-08-27 22:35:58 +0900

    update

--- design_pattern/decorator/main.cpp
+++ design_pattern/decorator/main.cpp
@@ -2,12 +2,12 @@
 #include<boost/format.hpp>
 #include<string>
 
-#include"coffee_base.h"
+#include"coffee_component.h"
 
 using namespace std;
 
-/// 
-class coffee_black : public coffer_base{
+/// Concrete Component 
+class coffee_black : public coffee_component{
 private:
   string m_name = "Black Coffee";
   int    m_cost = 10;
@@ -16,21 +16,31 @@ public:
   int    cost()   { return m_cost; }
 };
 
-///
-class coffee_with_sugger : public coffer_base{
-private:
-  string m_name = "Coffee";
-  int    m_cost = 5;
+/// Concrete Decorator
+class coffee_with_sugger : public coffee_decorator{
 public:
-  string getName(){ return m_name; }
-  int    cost()   { return m_cost; }
+  coffee_with_sugger(coffee_component *pbase):coffee_decorator(pbase){}
+
+  string getName(){ 
+    return m_pcomp->getName()+" with Sugger"; 
+  }
+  
+  int cost() {
+    return m_pcomp->cost()+5; 
+  }
 };
-
-
-
 
 
 ///
 int main(){
+  coffee_black       cf;
+  coffee_with_sugger cfs(new coffee_black);
+
+
+  cout << cf.getName()  << endl; /// "Black Coffee"
+  cout << cf.cost()     << endl; /// "10"
+
+  cout << cfs.getName() << endl; /// "Black Coffee with Sugger"
+  cout << cfs.cost()    << endl; /// "15"
 }
 
