cc496e0e860b3faf1086bffa52dbcd1f4e04e3a0
ORYZAPAO <takehitoy@gmail.com>
2016-09-04 16:15:42 +0900

    Update

--- design_pattern/factory_method/main.cpp
+++ design_pattern/factory_method/main.cpp
@@ -4,10 +4,56 @@
 #include<product.h>
 #include<factory.h>
 
+using namespace std;
 
-class Wheat : public Product{
+//
+class Juice : public Product{
+public:
+  Juice():Product(nullptr){
+    cout << boost::format("ジュース\n");
+  };
+  
+  ~Juice(){};  
+};
+
+//
+class Milk : public Product{
+public:
+  Milk():Product(nullptr){
+    cout << boost::format("牛乳\n");
+  };
+  
+  ~Milk(){};  
+};
+
+
+//
+class MilkFactory : public Factory{
+ public:
+  virtual Product *createProduct(){
+    cout << boost::format("牛乳を製造\n");
+    return( new Milk() );    
+  }
+};
+
+
+//
+class JuiceFactory : public Factory{
+ public:
+  virtual Product *createProduct(){
+    cout << boost::format("ジュースを製造\n");
+    return( new Juice() );    
+  }
 };
 
 
 int main(){
+  JuiceFactory  fj;
+  MilkFactory   fm;
+
+  Product      *p1;
+  Product      *p2;
+
+  p1 = fj.createProduct();
+  p2 = fm.createProduct();
 }
