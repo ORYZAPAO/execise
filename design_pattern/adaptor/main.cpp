471a17347eea78c113ab178cf14ac5c6de70d0b7
oryzapao <oryzapao@gmail.com>
2016-09-09 23:40:45 +0900

    1st commot

---
+++ design_pattern/adaptor/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include<string>
+
+#include <boost/format.hpp>
+
+using namespace std;
+
+//using namespace myspace;
+
+//------------------------------------------------------------
+int main(){
+
+
+}
