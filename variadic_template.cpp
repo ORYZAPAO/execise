3787dfa275a230e3d33f8debf7986a8a8271d95b
oryzapao <oryzapao@gmail.com>
2017-12-11 22:52:40 +0900

    1st Commit

---
+++ variadic_template.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<boost/format.hpp>
+
+using namespace std;
+
+// ----------------------------------------
+// 可変個引数テンプレート
+// ----------------------------------------
+void f(){}
+
+template<typename T, typename... Tail>
+void f(T head, Tail... tail){
+  cout << head << " ";
+
+  f(tail...);  // 再帰的に呼び出す。
+}
+
+
+// ----------------------------------------
+// ----------------------------------------
+int main(){
+  // "犬 猫 3 とり"  
+  f("犬", "猫", 3, "とり");
+  cout << endl;
+}
