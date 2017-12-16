54e549ff10dec9197fac9631095edbfddd076f96
oryzapao <oryzapao@gmail.com>
2017-12-16 17:18:27 +0900

    Range Sample

---
+++ range.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+#include<boost/format.hpp>
+
+using namespace std;
+
+// ----------------------------------------
+// Range Sample
+// ----------------------------------------
+int main(){
+
+  //------------------------------
+  // C言語由来の配列 と Range
+  //------------------------------
+  cout << "Step.1) c array and Rabge" << endl;
+  int  ll[] = {0,1,2,3,4,5,6,7}; // 従来のC形式の配列
+
+  
+  cout << "  Scope For" << endl;
+  for(auto &d : ll){   // 範囲for文が、使用可
+    cout << " " <<  d;
+  }
+  cout << endl << endl;
+
+  
+  //------------------------------
+  // C++ の vector<> と Range
+  //------------------------------
+  cout << "Step.2) c++ vector<> and Range" << endl;
+  vector<int> vec ={0,1,2,3,4,5,6,7}; // 従来のC形式の配列
+
+  cout << " * Scope For" << endl;
+  for(auto &d : vec){   // 範囲for文
+    cout << " " << d;
+  }
+  cout << endl;
+
+  
+  cout << " * begin()-end() Roop" << endl;
+  for(auto i=vec.begin(); i<vec.end(); i++){   // begin() - end() でループ
+    cout << " " << *i;
+  }
+  cout << endl;
+  
+}
