0bb31aea94f1ac2716efa9e704a508b47cd33f21
ORYZAPAO <oryzapao@gmail.com>
2018-01-06 21:44:34 +0900

    Add std::forward

--- remove_reference_KARI.hpp
+++ remove_reference_KARI.hpp
@@ -46,4 +46,33 @@ namespace MY{
     return static_cast<typename std::remove_reference<_Tp>::type&&>(__t);
   }
 
+
+  /**
+   *  @brief  Forward an lvalue.
+   *  @return The parameter cast to the specified type.
+   *
+   *  This function is used to implement "perfect forwarding".
+   */
+  template<typename _Tp>
+    constexpr _Tp&&
+    forward(typename std::remove_reference<_Tp>::type& __t) noexcept
+    { return static_cast<_Tp&&>(__t); }
+
+  /**
+   *  @brief  Forward an rvalue.
+   *  @return The parameter cast to the specified type.
+   *
+   *  This function is used to implement "perfect forwarding".
+   */
+  template<typename _Tp>
+    constexpr _Tp&&
+    forward(typename std::remove_reference<_Tp>::type&& __t) noexcept
+    {
+      static_assert(!std::is_lvalue_reference<_Tp>::value, "template argument"
+		    " substituting _Tp is an lvalue reference type");
+      return static_cast<_Tp&&>(__t);
+    }
+
+
+  
 };
