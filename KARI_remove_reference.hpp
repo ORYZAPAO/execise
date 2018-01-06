6be9197e7643a27bb6df0ca9f197eda2ede9da2f
ORYZAPAO <oryzapao@gmail.com>
2018-01-06 18:27:20 +0900

    1st Commit remove_reference

---
+++ KARI_remove_reference.hpp
@@ -0,0 +1,32 @@
+// 
+//[from] /usr/include/c++/7.2.1/type_traits
+//
+
+// Reference transformations.
+
+  /// remove_reference
+  template<typename _Tp>
+    struct remove_reference
+    { typedef _Tp   type; };
+
+  template<typename _Tp>
+    struct remove_reference<_Tp&>
+    { typedef _Tp   type; };
+
+  template<typename _Tp>
+    struct remove_reference<_Tp&&>
+    { typedef _Tp   type; };
+
+#if __cplusplus > 201103L
+  /// Alias template for remove_reference
+  template<typename _Tp>
+    using remove_reference_t = typename remove_reference<_Tp>::type;
+
+  /// Alias template for add_lvalue_reference
+  template<typename _Tp>
+    using add_lvalue_reference_t = typename add_lvalue_reference<_Tp>::type;
+
+  /// Alias template for add_rvalue_reference
+  template<typename _Tp>
+    using add_rvalue_reference_t = typename add_rvalue_reference<_Tp>::type;
+#endif
