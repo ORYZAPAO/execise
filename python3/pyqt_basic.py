007acd62dc2f5cde3a9436a6a2918b4698102304
ORYZAPAO <oryzapao@gmail.com>
2018-11-11 19:04:10 +0900

    Update

--- python3/pyqt_basic.py
+++ python3/pyqt_basic.py
@@ -1,14 +1,23 @@
+# coding: utf-8
+#
+
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
+
+class ExampleWidget(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.resize(640, 480)
+        #self.move(300, 300)
+        self.setWindowTitle('sample')
+        self.show()
 
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
-    widget = QWidget()
-    widget.resize(250, 150)
-    widget.move(300, 300)
-    widget.setWindowTitle('sample')
-    widget.show()
-
-    sys.exit(app.exec_())
-      
+    ew = ExampleWidget()    
+    sys.exit(app.exec_()) 
