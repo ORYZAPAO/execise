1d3554b9755f94adcbc7452aace9fe3540b9e125
ORYZAPAO <oryzapao@gmail.com>
2018-12-02 23:14:43 +0900

    Web Scraping m Beautiful Soup

---
+++ python3/webscraping.py
@@ -0,0 +1,30 @@
+# coding: UTF-8
+#import urllib
+import urllib.request
+from bs4 import BeautifulSoup
+
+# アクセスするURL
+url = "http://www.nikkei.com/"
+
+# URLにアクセスする htmlが帰ってくる → <html><head><title>経済、株価、ビジネス、政治のニュース:日経電子版</title></head><body....
+html = urllib.request.urlopen(url).read()
+
+# htmlをBeautifulSoupで扱う
+soup = BeautifulSoup(html, "html.parser")
+
+
+lst = soup.select("h2")
+for article in lst:
+    print(article)
+    
+
+
+# タイトル要素を取得する → <title>経済、株価、ビジネス、政治のニュース:日経電子版</title>
+title_tag = soup.title
+
+# 要素の文字列を取得する → 経済、株価、ビジネス、政治のニュース:日経電子版
+title = title_tag.string
+
+# タイトル要素を出力
+print(title_tag)
+print(title)
