5081489b4d007913d28aa49f5e68452f0d9bf1bc
ORYZAPAO <oryzapao@gmail.com>
2019-01-07 00:43:18 +0900

    n-base number Sample

---
+++ lisp/base.lisp
@@ -0,0 +1,9 @@
+;; Reader Macro: #b
+;;     2進数による数を表現します。
+;; Reader Macro: #o
+;;     8進数による数を表現します。
+;; Reader Macro: #x
+;;     16進数による数を表現します。
+;; Reader Macro: #Nr
+;;     N進数による数を表現します。 
+(print #b1010) ; 10
