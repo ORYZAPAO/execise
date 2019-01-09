8eed055e28af1cca3b10b5ea9aae0ca45e178a40
ORYZA <oryzapao@gmail.com>
2019-01-09 22:52:59 +0900

    Remove lisp

--- lisp/base.lisp
+++
@@ -1,9 +0,0 @@
-;; Reader Macro: #b
-;;     2進数による数を表現します。
-;; Reader Macro: #o
-;;     8進数による数を表現します。
-;; Reader Macro: #x
-;;     16進数による数を表現します。
-;; Reader Macro: #Nr
-;;     N進数による数を表現します。 
-(print #b1010) ; 10
