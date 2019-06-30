6a35879eaf64240444e12bd5664685dd07cb281b
ORYZA <oryzapao@gmail.com>
2019-06-30 20:13:32 +0900

    Update

--- class_template.cpp
+++ class_template.cpp
@@ -18,10 +18,9 @@ private:
   T0 dat;
 
 public:  
+
   // --------------------------------------------------
-  // クラステンプレート
-  //
-  // メンバテンプレート関数（コンストラクタ）の宣言＋定義
+  //コンストラクタ宣言＋定義
   template<typename T1=int> 
   MyClass():dat(nullptr){
     cout << "[Constructor] dat(" << dat << ")" << endl;
@@ -40,7 +39,7 @@ public:
   
   // --------------------------------------------------
   // メンバテンプレート関数の宣言
-  template<typename T1> void func_template();
+  template<typename T1> void func_template(T1 dat2);
 
 };
 
@@ -53,8 +52,9 @@ void MyClass<T0>::func(){
 // メンバテンプレート関数の定義（クラス定義の外で、定義）
 template<typename T0> // <-- クラスのテンプレート
 template<typename T1> // <-- メンバ関数のテンプレート
-void MyClass<T0>::func_template(){
-  cout << "[func_template] dat(" << dat << ")" << endl;
+void MyClass<T0>::func_template(T1 dat2){
+  cout << "[func_template] dat(" << dat << "), "
+                      << "dat2(" << dat2 << ")" << endl;
 }
 
 
@@ -63,27 +63,38 @@ int main(){
 
   // MyClass<string>(string d, float *d2=f0)
   //
-  //   MyClass<T0=string>型のコンストラクタ
-  //     MyClass<T0=string>(<T0=string d>, <T1=float> *) を呼び出す
+  //   MyClass<string>型のコンストラクタ
+  //   func_template<>(<T1=int>) 
   //
   string  s0("MacOS");
   float   f0(100.0f);
   MyClass<string> a(s0, &f0);  // "[Constructor] dat(MacOS), ptr(0x7fff5a69b5c4)"
   a.func();                    // "[func] dat(MacOS)"
-  a.func_template<int>();      // "[func_template] dat(MacOS)"
-
+  a.func_template<int>(100);   // "[func_template] dat(MacOS)"
   cout << endl;
   
   // MyClass<int>(int d, int *d2=nullptr)
   //
-  //   MyClass<T0=int>型のコンストラクタ
-  //     MyClass<T0=int>(<T0=int> d, <T1=float> *) を呼び出す
+  //   MyClass<int>型のコンストラクタ
+  //   func_template<>(<T1=int>) 
   //
-  MyClass<int>    b(999);      // [Constructor] dat(999), ptr(0x0)
-  b.func();                    // [func] dat(999)
-  b.func_template<int>();      // [func_template] dat(999)
-
+  MyClass<int>    b(111);      // [Constructor] dat(111), ptr(0)
+  b.func();                    // [func] dat(111)
+  b.func_template<int>(200);   // [func_template] dat(111), dat2(200)
   cout << endl;
 
+
+  // MyClass<int>(int d, int *d2=nullptr)
+  //
+  //   MyClass<int>型のコンストラクタ
+  //   func_template<>(<T1=string>) 
+  //
+  MyClass<int>    c(333);                             // [Constructor] dat(333), ptr(0)
+  c.func();                                           // [func] dat(333)
+  c.func_template<std::string>(std::string("jjjj"));  // [func_template] dat(333), dat2(jjjj)
+  cout << endl;
+
+
+  
   return 0;
 }
