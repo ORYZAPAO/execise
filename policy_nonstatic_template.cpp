8f53ce977819eaee80f9758151e5318713444fcb
ORYZA <oryzapao@gmail.com>
2019-10-06 20:40:56 +0900

    Sample for Policy Class

--- policy_nonstatic_template.cpp
+++ policy_nonstatic_template.cpp
@@ -3,7 +3,7 @@
 //
 //  ポリシーとポリシークラス(static)
 //
-//  ポリシークラスが「内部状態を持たない」場合に使うとよい
+//  ポリシークラスが「内部状態を持つ」場合に使うとよい
 // ------------------------------------------------------------
 #include<iostream>
 #include<boost/format.hpp>
@@ -15,13 +15,18 @@ using namespace std;
 // ポリシーを定義
 //
 struct Policy_A{
-  static void func(){
+  bool item0; // なんらかの状態を保存
+
+  void func(){
     std::cout << "Call Policy_A::func()\n";
   }
 };
 
 struct Policy_B{
-  static void func(){
+  bool item0; // なんらかの状態を保存
+  bool item1; //
+
+  void func(){
     std::cout << "Call Policy_B::func()\n";
   }
 };
@@ -31,18 +36,24 @@ struct Policy_B{
 // 呼出し
 //
 template<class Policy>
-struct hoge{
-  static void foo(){
-    Policy::func();
+struct hoge{  
+  Policy p;
+  
+  void foo(){
+    p.func();
   }
 };
 
 
 // ------------------------------------------------------------
 int main(){
-  cout << boost::format("policy_static_template.cpp\n");
+  cout << boost::format("policy_nonstatic_template.cpp\n");
 
-  // 
-  hoge<Policy_A>::foo(); // "Call Policy_A::func()";
-  hoge<Policy_B>::foo(); // "Call Policy_B::func()";
+  //hoge<Policy_A>::foo(); // "Call Policy_A::func()";
+  //hoge<Policy_B>::foo(); // "Call Policy_B::func()";
+
+  hoge<Policy_A> a;
+  hoge<Policy_B> b;
+  a.foo(); // "Call Policy_A::func()"
+  b.foo(); // "Call Policy_B::func()"
 }
