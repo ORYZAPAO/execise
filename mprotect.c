2cd91e148b6e351a5e9c6dd3f5fd69771b027c9f
ORYZAPAO <oryzapao@gmail.com>
2023-06-26 21:33:47 +0900

    Update

--- mprotect.c
+++ mprotect.c
@@ -1,25 +1,32 @@
+/*
+ * ヒープ領域上のコードを実行する
+ */
+
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include <sys/mman.h>
 #include <unistd.h>
 
+// コピー対象のコード
 double pi(void){
   return 3.14159f;
 }
 
 
 void alloc_execution(void *pi_pt){
-  puts("[alloc_execution()]\n");;
-  printf("pi_pt Address: %p\n", pi_pt);
-  
+  puts("[alloc_execution()]");;
+
+  // ページサイズ単位
   size_t pagesize = sysconf(_SC_PAGESIZE);
   // size_t  pagesize = getpagesize();
 
   void  *p = (void*)((long long)pi_pt & ~(pagesize - 1)); // ページ境界に揃える
-  printf("  Page Size : %u\n" , pagesize);
-  printf("  p         : %x\n", p);
+  printf("  Page Start: %x\n", p);
+  printf("       Size : %u\n", pagesize);
   
+  // mprotect() で、ヒープ領域にRead,Write,Execute属性を指定
   if( mprotect(p, pagesize*10L, PROT_READ | PROT_WRITE | PROT_EXEC) != 0 ){
     printf("  mprotect() Failed\n");
   }
@@ -29,12 +36,18 @@ void alloc_execution(void *pi_pt){
 int main(){
   const size_t fsize = 4096;
   printf("[main()]\n");
-  
+
+  // ヒープ領域を確保して、コード(pi()) をヒープへコピー
   void  *pi_pt = malloc(fsize);
-  memcpy(pi_pt, pi, fsize);
+  memcpy(pi_pt, (const void *)pi, fsize);
+  printf("  pi()    Address: %p\n", pi);
+  printf("  pi_pt() Address: %p\n", pi_pt);
+
+  // ヒープ領域に実行権を与える
   alloc_execution((void *)pi_pt);
 
-  printf("Answer ==> %f\n", ((double (*)(void))pi_pt)() );
+  // ヒープ領域のコードを実行
+  printf("Answer ==> pi_pt() = %f\n", ((double (*)(void))pi_pt)() );
 
   free(pi_pt);
 }
--- mprotect2.cpp
+++ mprotect2.cpp
@@ -1,43 +1,60 @@
+/*
+ * ヒープ領域上のコードを実行する
+ */
+
 #include<iostream>
 #include<boost/format.hpp>
 #include<string.h>
 #include <sys/mman.h>
 #include <unistd.h>
 
-double pi(){
+// コピー対象のコード
+extern "C" double pi(){
+  //std::cout << "[alloc_execution()]" << std::endl;
+  //return 0xABC;
   return 3.14159f;
 }
 
+
 void alloc_execution(void *pi_pt){
   std::cout << "[alloc_execution()]" << std::endl;
-  std::cout << boost::format("pi_pt Address: %p") % pi_pt << std::endl;
   
+  // ページサイズ単位
   size_t pagesize = sysconf(_SC_PAGESIZE);
   // size_t  pagesize = getpagesize();
 
   void  *p = (void*)((long long)pi_pt & ~(pagesize - 1)); // ページ境界に揃える
-  std::cout << "  Page Size : " << pagesize << std::endl;
-  std::cout << "  p         : " << p  << std::endl;
+  std::cout << "  Page Start: " << p  << std::endl;
+  std::cout << "       Size : " << pagesize << std::endl;
   
+  // mprotect() で、ヒープ領域にRead,Write,Execute属性を指定
   if( mprotect(p, pagesize*10L, PROT_READ | PROT_WRITE | PROT_EXEC) != 0 ){
     std::cout << "  mprotect() Failed" << std::endl;
   }
-  
 }
 
 int main(){
   const size_t fsize = 4096;
   std::cout << "[main()]" << std::endl;
   
+  // ヒープ領域を確保して、コード(pi()) をヒープへコピー
   char  *pi_pt = new char[fsize];
   memcpy(pi_pt, reinterpret_cast<const void*>(pi), fsize);
-  //memcpy(pi_pt, pi, fsize);
-  alloc_execution((void *)pi_pt);
+  std::cout << boost::format("  pi()    Address : %x") % reinterpret_cast<const void*>(pi) << std::endl;
+  std::cout << boost::format("  pi_pt() Address : %x") % reinterpret_cast<const void*>(pi_pt) << std::endl;
 
-  //std::cout << "Anser ==> " << 
-  //   reinterpret_cast<double (*)()>(pi_pt)() << std::endl;
-  printf("Answer ==> %f\n", ((double (*)(void))pi_pt)() );
-  printf("Answer ==> %p %p\n", &pi, pi_pt );
+  // ヒープ領域に実行権を与える
+  alloc_execution(reinterpret_cast<void *>(pi_pt));
+
+  // ヒープ領域のコードを実行
+  std::cout << "Answer ==> pi_pt() = " <<  std::fixed  <<
+    reinterpret_cast<double (*)()>(pi_pt)() << std::endl;
+
+  std::cout << "Answer ==> pi() = " <<  std::fixed  <<
+    pi() << std::endl;
 
   delete [] pi_pt;
+
+
+
 }
